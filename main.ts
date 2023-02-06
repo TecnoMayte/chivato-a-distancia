4be46b6d400f897c668c29776b52489cf7d29c8e
TecnoMayte <114059096+TecnoMayte@users.noreply.github.com>
2023-02-06 22:43:46 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,32 @@
 /**
  * RECEPTOR
  */
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber > 128) {
+        basic.showString("1")
+    } else {
+        basic.clearScreen()
+    }
+    if (receivedNumber < 128) {
+        basic.showString("2")
+    } else {
+        basic.clearScreen()
+    }
 })
 radio.setGroup(1)
 let habitación1 = input.lightLevel()
 let habitación2 = input.lightLevel()
 /**
- * EMISOR 1
- */
-basic.forever(function () {
-    if (habitación1 < 130) {
-        radio.sendString("1")
-        basic.pause(1000)
-        basic.clearScreen()
-    }
-})
-/**
  * EMISOR 2
  */
 basic.forever(function () {
-    if (habitación2 < 100) {
-        radio.sendString("2")
-        basic.pause(1000)
-        basic.clearScreen()
-    }
+    radio.sendNumber(input.lightLevel())
+    basic.pause(1000)
+})
+/**
+ * EMISOR 1
+ */
+basic.forever(function () {
+    radio.sendNumber(input.lightLevel())
+    basic.pause(1000)
 })
